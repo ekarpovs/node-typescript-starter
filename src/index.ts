61dd2cba42f9009535778287b087ce7290600d0c
evgeny <ekarpovsky@gmail.com>
2023-10-15 10:57:20 +0000

    Test deploy to cyclic

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-export const greet = () => {
-  return 'Hello World!';
-};
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import express, { Express, Request, Response, Router } from 'express';
 
-console.log('run');
+dotenv.config();
+
+const app: Express = express();
+
+app.use(bodyParser.json());
+
+// Routers Setup
+const router = Router();
+
+router.get('/', (_req: Request, res: Response) => {
+  res.json({ message: 'Hello world!' });
+});
+
+app.use(router);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
